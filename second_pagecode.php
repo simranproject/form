3e50b840dddd41bff61f8bae8029b6b40af828a0
simranproject <simran.srivastava246@gmail.com>
2025-11-13 22:07:31 +0530

    Updates files with latest changes

--- second_pagecode.php
+++ second_pagecode.php
@@ -6,7 +6,7 @@ if(isset($_POST['step2btn'])){
  $a=$_POST['num'];
  $b=$_POST['address'];
 
- $conn=mysqli_connect("db4free.net", "simran123", "Simran@123", "formdb", 3306);
+ $conn=mysqli_connect("db4free.net", "localhost", "root", "", 3306);
  $up="update formtbl set mobile='$a',city='$b' where id=$lastid";
 
  if(mysqli_query($conn,$up)){
