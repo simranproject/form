5ff86d16f321054e21f024992d53757452e0290c
simranproject <simran.srivastava246@gmail.com>
2025-11-14 16:00:11 +0530

    Updates files with latest changes

--- previous_pagecode.php
+++ previous_pagecode.php
@@ -7,7 +7,7 @@ if(isset($_POST['stepbtn']))
     $a=$_POST['first_name'];
     $b=$_POST['middle_name'];
     $c=$_POST['last_name'];
-    $conn=mysqli_connect("localhost", "root", "simran","form");
+    $conn=mysqli_connect("localhost", "root", "","form",3306);
     $up="update formtbl set first_name='$a',middle_name='$b',last_name='$c' where id=$id";
 
     if(mysqli_query($conn,$up))
